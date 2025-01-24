22036a4f3d6928a29475884d2a17521e1bf227bc
Diego Ysrael Antonio <https://github.com/DiegoAntonio04/Antonio_DSAMidterms.git>
2025-01-24 22:09:58 +0800

    reverted back

--- src/PokeDex.jsx
+++ src/PokeDex.jsx
@@ -60,7 +60,7 @@ function Login() {
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify(formData),
+        body: JSON.stringify(initialFormData),
       });
 
       if (response.ok) {
