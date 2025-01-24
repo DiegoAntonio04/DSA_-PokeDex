22dac27ae8ec47bbb1bf02e6c11ecd35f8715a0c
Diego Ysrael Antonio <https://github.com/DiegoAntonio04/Antonio_DSAMidterms.git>
2025-01-24 17:38:58 +0800

    edited await

--- src/PokeDex.jsx
+++ src/PokeDex.jsx
@@ -54,7 +54,7 @@ function Login() {
     }
 
     try {
-      const response = await fetch("https://antoniopokemon.azurewebsites.net/submit", {
+      const response = await fetch("https://antoniopokemon.azurewebsites.net/", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
